7701204a6e57f9a1b52102c08edcfd155b1ae147
tungkw <914989053@qq.com>
2021-01-01 18:48:59 +0800

    camera noise

--- sim_class/realsense_d435.py
+++ sim_class/realsense_d435.py
@@ -1,16 +1,15 @@
 from .vision_sensor import VisionSensor
 from .proximity_sensor import ProximitySensor
 import numpy as np
-import cv2
 
 class RealsenseD435(VisionSensor):
     def __init__(self, client, name=None, handle=None):
         super().__init__(client, name=name, handle=handle)
-        farest_point = np.array([[0,0,1]]) @ np.linalg.inv(self.intrinsic).T * self.far_plane
-        print(np.linalg.inv(self.intrinsic))
-        print(np.array([[0,0,1]]) @ np.linalg.inv(self.intrinsic))
-        print(farest_point)
-        print(np.linalg.norm(farest_point))
+        # farest_point = np.array([[0,0,1]]) @ np.linalg.inv(self.intrinsic).T * self.far_plane
+        # print(np.linalg.inv(self.intrinsic))
+        # print(np.array([[0,0,1]]) @ np.linalg.inv(self.intrinsic))
+        # print(farest_point)
+        # print(np.linalg.norm(farest_point))
         # res, normal_sensor_handle = self.client.simxExecuteScriptString(
         #     "sim.createProximitySensor("
         #     "   sim.proximitysensor_ray_subtype, "
@@ -36,18 +35,19 @@ class RealsenseD435(VisionSensor):
 
     def get_depth(self, with_normal=False):
         depth = super(RealsenseD435, self).get_depth()
-        cv2.imshow("depth_", depth)
         depth = self.add_noise(depth)
         return depth
 
     def add_noise(self, z):
         h, w = z.shape
-
-        # axial noise
         I, J = np.meshgrid(np.arange(h), np.arange(w), indexing='ij')
         points = np.stack([J, I, np.ones_like(J)], axis=-1)
         points = points @ np.linalg.inv(self.intrinsic).T
         points = points * np.expand_dims(z, axis=-1)
+        z[z>=self.far_plane-0.01] = 0
+
+
+        # axial noise
         # theta_y
         points_l, points_r = np.copy(points), np.copy(points)
         points_l[:, 1:, :] = points[:, :-1, :]
@@ -65,15 +65,34 @@ class RealsenseD435(VisionSensor):
         theta_x = np.arccos(cos_theta_x)
         theta_x[theta_x>np.pi/2] = np.pi - theta_x[theta_x>np.pi/2]
         # axial noise model
-        sigma_z = 0.00163 + 0.0007278*z + 0.003949*z**2 + 0.022*z**1.5*theta_y/(np.pi/2-theta_y)**2 + 0.022*z**1.5*theta_x/(np.pi/2-theta_x)**2
+        sigma_z = 0.00163 + 0.0007278*z + 0.003949*(z**2)\
+                  + 0.022*(z**1.5)*theta_y/((np.pi/2-theta_y)**2)\
+                  + 0.022*(z**1.5)*theta_x/((np.pi/2-theta_x)**2)
+        z = z + np.random.normal(scale=sigma_z)
+        z[np.logical_or(z>self.far_plane, z<self.near_plane)] = 0
+
+        # random miss
+        missing_mask = np.logical_or(np.random.uniform(size=z.shape) > cos_theta_x**0.1, np.random.uniform(size=z.shape) > cos_theta_y**0.1)
+        missing_mask = np.logical_and(missing_mask, np.random.uniform(size=z.shape) > z/self.far_plane)
+        z[missing_mask] = 0.0
 
         # lateral noise
-        grey = (z/1 * 255).astype(np.uint8)
-        edges = cv2.Canny(grey, 100, 200)
-        sigma_z[edges>0] = sigma_z[edges>0] + 0.0407
+        sigma_l = np.zeros_like(z)
+        sigma_l[z>0] = 0.00432
+        points_n = np.copy(points)
+        points_n[..., 0] = points_n[..., 0] + np.random.normal(scale=sigma_l)
+        points_n[..., 1] = points_n[..., 1] + np.random.normal(scale=sigma_l)
+        points_n = points_n / points_n[..., [2]] @ self.intrinsic.T
+        u = points_n[..., 0]
+        v = points_n[..., 1]
+        # u = np.copy(J).astype(float) + np.random.normal(scale=sigma_l)
+        # v = np.copy(I).astype(float) + np.random.normal(scale=sigma_l)
+        valid_j = np.logical_and(u>=0, u<self.xdim)
+        u[~valid_j] = J[~valid_j]
+        valid_i = np.logical_and(v>=0, v<self.ydim)
+        v[~valid_i] = I[~valid_i]
+        z = z[v.astype(I.dtype), u.astype(J.dtype)]
 
-        z = z + np.random.normal(0, sigma_z)
-        z[np.logical_or(z>self.far_plane, z<self.near_plane)] = self.far_plane
         return z
 
     def get_normal(self, points):
